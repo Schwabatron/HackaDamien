b7ac020f6d03deb63a7e1585761b8539d79f4c1c
CarmaClisthert <mariela.santos.ptech@gmail.com>
2024-04-14 05:09:36 -0400

    Animation Hell
    Almost all the animations are done even though some of them are shit

--- Assets/PlayerScripts/PlayerMovement.cs
+++ Assets/PlayerScripts/PlayerMovement.cs
@@ -8,15 +8,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed = 8f;
     private bool jumping = false;
+    public Animator animator;
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        spriteRenderer.flipX = rb.velocity.x < 0f;
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && jumping == false)
         {
@@ -26,6 +31,23 @@ public class PlayerMovement : MonoBehaviour
         if (rb.velocity.y == 0)
         {
             jumping = false;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space)) || Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("isJumping", true);
+        }else
+        {
+            animator.SetBool("isJumping", false);
+        }
+
+        if (rb.velocity.y > 0.5)
+        {
+            animator.SetBool("isFalling", true);
+        }
+        if (rb.velocity.y <= 0.5)
+        {
+            animator.SetBool("isFalling", false);
         }
     }
 }
