19d8e2a7f4b9a4d6d2b69c00ef7a673b4b71abcb
Tyler-MTLJ <166947362+Tyler-MTLJ@users.noreply.github.com>
2024-04-13 23:42:04 -0400

    added key and ladder and barricade script

--- Assets/KeyScript/GetKey.cs
+++ Assets/KeyScript/GetKey.cs
@@ -6,11 +6,6 @@ using UnityEngine.Events;
 public class GetKey : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    public bool isInRange;
-    public KeyCode interactKey;
-    public UnityEvent interactAction;
-
     void Start()
     {
         
@@ -20,5 +15,40 @@ public class GetKey : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //whenever you collide with something
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if you touch key
+        if (collision.gameObject.name.Equals("Key"))
+        {
+            //remove the key
+            Destroy(collision.gameObject);
+
+            //create ladder and add components
+            GameObject ladder = new GameObject("ladder");
+            ladder.tag = "Ladder";
+
+            //create SpriteRenderer and add to ladder
+            SpriteRenderer sr = new SpriteRenderer();
+            sr = ladder.AddComponent<SpriteRenderer>();
+
+            //create boxcollider2d and add to ladder as well as configure
+            BoxCollider2D bc = new BoxCollider2D(); 
+            bc = ladder.AddComponent<BoxCollider2D>();
+            //bc config
+            bc.isTrigger = true;
+            bc.size = new Vector2(.7f, .65f);
+            //set ladder sprite as a square
+            Texture2D texture = new Texture2D(64, 64);
+            sr.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one * 0.5f);
+
+            //setup ladder size and position
+            ladder.transform.position = new Vector2(12, 3);
+            ladder.transform.localScale = new Vector2(3, 11);
+
+        }
+    
     }
 }
